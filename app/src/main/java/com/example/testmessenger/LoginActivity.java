806b47abe432c54cbddbb29f02dfe401537d4cf6
андрей <andrej.korolev.04@mail.ru>
2025-07-16 23:09:43 +0300

    LoginViewModel + Добавление логики разлогина

--- app/src/main/java/com/example/testmessenger/LoginActivity.java
+++ app/src/main/java/com/example/testmessenger/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.testmessenger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,17 +23,25 @@ public class LoginActivity extends AppCompatActivity {
     private TextView textViewForgotPassword;
     private TextView textViewRegister;
 
+    private LoginViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initViews();
+        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        observeViewModel();
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
-                // login
+                viewModel.login(email, password);
             }
         });
         textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -48,11 +62,36 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Intent intent = UsersActivity.newIntent(LoginActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+
     private void initViews() {
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
         textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
         textViewRegister = findViewById(R.id.textViewRegister);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context ,LoginActivity.class);
     }
 }
